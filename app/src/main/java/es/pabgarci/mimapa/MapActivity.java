eb189f6b901e30a5c15d1612bff447784380210d
pabgarci <100291959@alumnos.uc3m.es>
2015-09-28 11:14:20 +0200

    Added tittle to App

--- app/src/main/java/es/pabgarci/mimapa/MapActivity.java
+++ app/src/main/java/es/pabgarci/mimapa/MapActivity.java
@@ -207,7 +207,6 @@ public class MapActivity extends FragmentActivity implements
             }
         });
 
-
         builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
